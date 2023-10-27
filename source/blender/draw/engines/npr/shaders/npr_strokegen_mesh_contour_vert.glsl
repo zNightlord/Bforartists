def52b15524785da8e390f1f6fc85e99bba4be7d
Jiang <jwtstone@gmail.com>
2023-10-27 19:26:28 +0800

    Improve the vertex shader a bit

--- source/blender/draw/engines/npr/shaders/npr_strokegen_mesh_contour_vert.glsl
+++ source/blender/draw/engines/npr/shaders/npr_strokegen_mesh_contour_vert.glsl
@@ -9,17 +9,6 @@ void main()
     uint base_addr = vid * 6; 
     uint num_contour_edges = ssbo_bnpr_mesh_pool_counters_.num_contour_edges; 
 
-    /* Fetch vert pos from ssbo */
-    /* uint addr_vpos = mesh_pool_addr__wpos(contour_edge_id); 
-    if (vid % 2u == 1u) 
-        addr_vpos += 3u; 
-    vec3 pos_ws_curr = vec3(
-        uintBitsToFloat(buf_strokegen_mesh_pool[addr_vpos+0]), 
-        uintBitsToFloat(buf_strokegen_mesh_pool[addr_vpos+1]), 
-        uintBitsToFloat(buf_strokegen_mesh_pool[addr_vpos+2])
-    );
-    vec4 pos_ndc = point_world_to_ndc(pos_ws_curr); */
-
     uint addr_uv = mesh_pool_addr__edgeuv(contour_edge_id, num_contour_edges); 
     if (vid % 2u == 1u) 
         addr_uv += 2u;
@@ -52,7 +41,6 @@ void main()
     vec2 edgenor_uv = vec2(edgedir_uv.y, -edgedir_uv.x); 
     color = vec4(edgedir_uv.xy * 0.5f + .5f, 0, 1); 
 
-
     normal = vec3(0, 0, 1);
     tangent.xyz = vec3(0, 0, 1);
 
@@ -61,5 +49,7 @@ void main()
      * which can happen due to Z-fighting artifacts. */
     /* TODO: Further imporve this */
     gl_Position.z -= 1.0e-6 * whclip;
-    gl_Position.xy += edgenor_uv * whclip * pcs_screen_size_inv_ * 1.2f; 
+    gl_Position.xy += edgenor_uv * whclip * pcs_screen_size_inv_ * 1.0f; 
+	vec2 edge_dir_ext = (vid % 2u == 1u) ? edgedir_uv : -edgedir_uv; 
+	gl_Position.xy += edge_dir_ext * whclip * pcs_screen_size_inv_ * 1.5f; 
 }
