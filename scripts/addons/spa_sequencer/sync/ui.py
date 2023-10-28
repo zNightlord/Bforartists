ad3a52e748ac6bd073ea033e3c0d5c235f896cf3
Trung Phạm <61040487+zNightlord@users.noreply.github.com>
2023-10-28 16:52:34 +0700

    Update ui.py

--- scripts/addons/spa_sequencer/sync/ui.py
+++ scripts/addons/spa_sequencer/sync/ui.py
@@ -10,45 +10,41 @@ from spa_sequencer.utils import register_classes, unregister_classes
 class SEQUENCER_PT_SyncPanel(bpy.types.Panel):
     """Timeline Synchronization Panel."""
 
-    bl_label = "Timeline Synchronization"
+    bl_label = "Timeline Sync"
     bl_space_type = "SEQUENCE_EDITOR"
     bl_region_type = "UI"
-    bl_category = "Sequencer"
+    bl_region_type = 'HEADER'
 
     def draw(self, context):
-        self.layout.use_property_split = True
-        self.layout.use_property_decorate = False
+        layout = self.layout
+        col = layout.column()
+        col.use_property_split = True
+        col.use_property_decorate = False
         settings = get_sync_settings()
-        self.layout.operator(
+        col.operator(
             "wm.timeline_sync_toggle",
             text="Synchronize",
             icon="UV_SYNC_SELECT",
             depress=settings.enabled,
         )
-        self.layout.prop(settings, "master_scene")
+        col.prop(settings, "master_scene")
+        
+        col = layout.column()
+        box = col.box()
+        col = box.column()
+        col.enabled = bool(settings.master_scene)
+        self.draw_settings(col)
 
-
-class SEQUENCER_PT_SyncPanelAdvancedSettings(bpy.types.Panel):
-    """Timeline Synchronization advanced settings Panel."""
-
-    bl_label = "Advanced Settings"
-    bl_parent_id = "SEQUENCER_PT_SyncPanel"
-    bl_space_type = "SEQUENCE_EDITOR"
-    bl_region_type = "UI"
-    bl_category = "Sequencer"
-
-    def draw(self, context):
+    def draw_settings(self, layout):
         settings = get_sync_settings()
-        self.layout.prop(settings, "keep_gpencil_tool_settings")
-        self.layout.prop(settings, "bidirectional")
-        self.layout.prop(settings, "use_preview_range")
-        self.layout.prop(settings, "sync_all_windows")
-        self.layout.prop(settings, "active_follows_playhead")
-
+        layout.prop(settings, "keep_gpencil_tool_settings")
+        layout.prop(settings, "bidirectional")
+        layout.prop(settings, "use_preview_range")
+        layout.prop(settings, "sync_all_windows")
+        layout.prop(settings, "active_follows_playhead")
 
 classes = (
     SEQUENCER_PT_SyncPanel,
-    SEQUENCER_PT_SyncPanelAdvancedSettings,
 )
 
 
