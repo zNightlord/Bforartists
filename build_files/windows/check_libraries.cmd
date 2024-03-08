8cd9096f199c184b5c9c979984a2352e6fc3f0ce
Trung Phạm <61040487+zNightlord@users.noreply.github.com>
2024-03-08 11:24:32 +0700

    Update check_libraries.cmd

--- build_files/windows/check_libraries.cmd
+++ build_files/windows/check_libraries.cmd
@@ -10,37 +10,36 @@ if NOT EXIST "%BUILD_VS_LIBDIR%\.git" (
 		echo The required external libraries in %BUILD_VS_LIBDIR% are missing
 		echo.
 		set /p GetLibs= "Would you like to download them? (y/n)"
-		if /I "!GetLibs!"=="Y" (
-			echo.
-			echo Downloading %BUILD_VS_LIBDIR% libraries, please wait.
-			echo.
-			echo *********************************************************
-			echo *                                                       *
-			echo * Note: Once the initial download finishes and you see  *
-			echo *       "Resolving deltas: 100%% (nnn/nnn) done"         *
-			echo *       a second, much larger, update will occur with   *
-			echo *       no visible updates. Please do not interrupt     *
-			echo *       this process. It may take over an hour to       *
-			echo *       complete depending on your internet connection. *
-			echo *                                                       *
-			echo *********************************************************
-:RETRY
-			"%GIT%" -C "%BLENDER_DIR%\" config --local "submodule.%BUILD_VS_LIBDIR%.update" "checkout"
-			set GIT_LFS_SKIP_SMUDGE=1
-			"%GIT%" -C "%BLENDER_DIR%\" submodule update --progress --init "%BUILD_VS_LIBDIR%"
-			set GIT_LFS_SKIP_SMUDGE=
-			"%GIT%" -C "./%BUILD_VS_LIBDIR%" lfs pull
-			if errorlevel 1 (
-				set /p LibRetry= "Error during download, retry? y/n"
-				if /I "!LibRetry!"=="Y" (
-					goto RETRY
-				)
-				echo.
-				echo Error: Download of external libraries failed. 
-				echo Until this is resolved you CANNOT make a successful blender build.
-				echo.
-				exit /b 1
+		echo.
+		echo Downloading %BUILD_VS_LIBDIR% libraries, please wait.
+		echo.
+		echo *********************************************************
+		echo *                                                       *
+		echo * Note: Once the initial download finishes and you see  *
+		echo *       "Resolving deltas: 100%% (nnn/nnn) done"         *
+		echo *       a second, much larger, update will occur with   *
+		echo *       no visible updates. Please do not interrupt     *
+		echo *       this process. It may take over an hour to       *
+		echo *       complete depending on your internet connection. *
+		echo *                                                       *
+		echo *********************************************************
+                :RETRY
+		"%GIT%" -C "%BLENDER_DIR%\" config --local "submodule.%BUILD_VS_LIBDIR%.update" "checkout"
+		set GIT_LFS_SKIP_SMUDGE=1
+		"%GIT%" -C "%BLENDER_DIR%\" submodule update --progress --init "%BUILD_VS_LIBDIR%"
+		set GIT_LFS_SKIP_SMUDGE=
+		"%GIT%" -C "./%BUILD_VS_LIBDIR%" lfs pull
+		if errorlevel 1 (
+			set /p LibRetry= "Error during download, retry? y/n"
+			if /I "!LibRetry!"=="Y" (
+				goto RETRY
 			)
+                        echo.
+		        echo Error: Download of external libraries failed. 
+		        echo Until this is resolved you CANNOT make a successful blender build.
+		        echo.
+		        exit /b 1
+		)
 		) else (
            echo Not downloading libraries, until this is resolved you CANNOT make a successful blender build.
            exit /b 1
