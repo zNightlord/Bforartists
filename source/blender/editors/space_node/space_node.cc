aff5950ebd28178394e7af16b3c04a4e4409c204
Trung Phạm <61040487+zNightlord@users.noreply.github.com>
2024-07-28 01:05:14 +0700

    Update space_node.cc

--- source/blender/editors/space_node/space_node.cc
+++ source/blender/editors/space_node/space_node.cc
@@ -416,13 +416,14 @@ static SpaceLink *node_create(const ScrArea * /*area*/, const Scene * /*scene*/)
   BLI_addtail(&snode->regionbase, region);
   region->regiontype = RGN_TYPE_ASSET_SHELF;
   region->alignment = RGN_ALIGN_BOTTOM;
+  region->flag |= RGN_FLAG_HIDDEN;
 
   /* asset shelf header */
   region = MEM_cnew<ARegion>("asset shelf header for node");
 
   BLI_addtail(&snode->regionbase, region);
   region->regiontype = RGN_TYPE_ASSET_SHELF_HEADER;
-  region->alignment = RGN_ALIGN_BOTTOM | RGN_SPLIT_PREV;
+  region->alignment = RGN_ALIGN_BOTTOM | RGN_ALIGN_HIDE_WITH_PREV;
   /* bfa end */
 
 
