c2b0e755bc873d577632731e45068f5abe0bd90b
Trung <0znightlord0@gmail.com>
2024-08-19 18:23:50 +0700

    attrib 2

--- source/blender/draw/engines/eevee/shaders/surface_frag.glsl
+++ source/blender/draw/engines/eevee/shaders/surface_frag.glsl
@@ -13,6 +13,8 @@
 #pragma BLENDER_REQUIRE(surface_lib.glsl)
 #pragma BLENDER_REQUIRE(volumetric_lib.glsl)
 #pragma BLENDER_REQUIRE(renderpass_lib.glsl)
+#pragma BLENDER_REQUIRE(eevee_attributes_lib.glsl)
+
 
 #ifdef EEVEE_DISPLACEMENT_BUMP
 
