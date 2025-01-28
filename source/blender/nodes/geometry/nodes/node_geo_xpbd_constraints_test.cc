ae4608d9a9f02bdcda996dfb0dffa64bb1ed4361
Lukas Tönne <lukas@blender.org>
2025-01-28 17:21:10 +0100

    Started friction test.

--- source/blender/nodes/geometry/nodes/node_geo_xpbd_constraints_test.cc
+++ source/blender/nodes/geometry/nodes/node_geo_xpbd_constraints_test.cc
@@ -111,4 +111,44 @@ TEST(xpbd_constraints, OffcenterContact)
   EXPECT_V4_NEAR(float4(0, 0, 0.5f, 0), delta_rotation, 1e-5f);
 }
 
+TEST(xpbd_constraints, ContactFriction)
+{
+  const float3 velocity1 = {1, 0, 0};
+  const float3 orig_velocity1 = {0, 0, 0};
+  const float3 angular_velocity1 = {0, 0, 0};
+  const float3 orig_angular_velocity1 = {0, 0, 0};
+  const float3 velocity2 = {0, 0, 0};
+  const float3 angular_velocity2 = {0, 0, 0};
+
+  const float3 local_position1 = {0, 0, 0};
+  const float3 local_position2 = {0, 0, 0};
+  const float3 normal = {0, 0, 1};
+
+  const float restitution = 0.0f;
+  const float friction = 0.5f;
+
+  float delta_lambda_restitution, delta_lambda_friction;
+  float3 delta_velocity;
+  float3 delta_angular_velocity;
+  xpbd_constraints::eval_velocity_contact(velocity1,
+                                          angular_velocity1,
+                                          orig_velocity1,
+                                          orig_angular_velocity1,
+                                          velocity2,
+                                          angular_velocity2,
+                                          local_position1,
+                                          local_position2,
+                                          normal,
+                                          restitution,
+                                          friction,
+                                          delta_lambda_restitution,
+                                          delta_lambda_friction,
+                                          delta_velocity,
+                                          delta_angular_velocity);
+  // EXPECT_EQ(0, delta_lambda_restitution);
+  // EXPECT_NEAR(, delta_lambda, 1e-5f);
+  // EXPECT_V3_NEAR(float3(0, 0, 2), delta_position, 1e-5f);
+  // EXPECT_V4_NEAR(float4(0, 0, 0, 0), delta_rotation, 1e-5f);
+}
+
 }  // namespace blender::nodes::tests
