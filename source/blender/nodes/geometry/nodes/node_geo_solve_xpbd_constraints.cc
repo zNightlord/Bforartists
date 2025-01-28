e6fbe202db9d967a3088007ea65145abf436a086
Lukas Tönne <lukas@blender.org>
2025-01-28 10:08:06 +0100

    Move ConstraintEvalParams back into the .cc file for now.

--- source/blender/nodes/geometry/nodes/node_geo_solve_xpbd_constraints.cc
+++ source/blender/nodes/geometry/nodes/node_geo_solve_xpbd_constraints.cc
@@ -201,7 +201,65 @@ static AttributeReader<T> lookup_or_warn(const GeoNodeExecParams &params,
   return attributes.lookup_or_default<T>(attribute_id, domain, default_value);
 }
 
-using xpbd_constraints::ConstraintEvalParams;
+struct ConstraintEvalParams {
+  /* TODO split this by EvaluationTarget, only either (positions + rotations) or (velocities +
+   * angular_velocities) are used. */
+  Array<float3> positions;
+  Array<math::Quaternion> rotations;
+  VArraySpan<float3> old_positions;
+  VArraySpan<math::Quaternion> old_rotations;
+
+  Array<float3> velocities;
+  Array<float3> angular_velocities;
+  VArraySpan<float3> orig_velocities;
+  VArraySpan<float3> orig_angular_velocities;
+
+  VArraySpan<float> position_weights;
+  VArraySpan<float> rotation_weights;
+
+  Span<float4x4> collider_transforms;
+  /* Collider transforms of the previous frame for computing velocity constraints. */
+  Span<float4x4> old_collider_transforms;
+
+  struct {
+    VArray<int> solver_group;
+    VArraySpan<int> points;
+    VArraySpan<float3> goals;
+  } position_goal;
+  struct {
+    VArray<int> solver_group;
+    VArraySpan<int> points;
+    VArraySpan<math::Quaternion> goals;
+  } rotation_goal;
+  struct {
+    VArray<int> solver_group;
+    VArraySpan<float> alpha;
+    VArraySpan<float> gamma;
+    VArraySpan<int> points1;
+    VArraySpan<int> points2;
+    VArraySpan<float> edge_length;
+  } stretch_shear;
+  struct {
+    VArray<int> solver_group;
+    VArraySpan<int> points1;
+    VArraySpan<int> points2;
+    VArraySpan<float3> darboux_vector;
+  } bend_twist;
+  struct {
+    VArray<int> solver_group;
+    VArraySpan<int> points;
+    VArraySpan<int> collider_index;
+    VArraySpan<float3> local_position1;
+    VArraySpan<float3> local_position2;
+    VArraySpan<float3> normal;
+
+    VArraySpan<float> friction;
+    VArraySpan<float> restitution;
+
+    /* Remember active contacts for later velocity update. */
+    bke::SpanAttributeWriter<bool> active;
+  } contact;
+};
 
 static const VArray<int> &constraints_solver_groups(const ConstraintEvalParams &params,
                                                     const ConstraintType type)
