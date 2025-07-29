5efdc1ba4343a089387ae1c3f0ec5689821b5879
Falk David <falk@blender.org>
2025-07-29 11:27:58 +0200

    Update comment

--- source/blender/editors/include/ED_screen_types.hh
+++ source/blender/editors/include/ED_screen_types.hh
@@ -21,7 +21,7 @@ struct ScreenAnimData {
   /** Do not read from this, only for comparing if region exists. */
   ARegion *region;
 
-  /* Scene and the View Layer that is playing. */
+  /* The Scene and the View Layer that the animation timer is playing. */
   Scene *scene;
   ViewLayer *view_layer;
   /* For sequencer scenes, account for scene syncing during playback. */
