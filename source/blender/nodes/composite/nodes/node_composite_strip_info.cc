23a5c1010ae4714ada4d6613d0b83d0435ea4d9a
Ramon Klauck <ramonklauck3@gmail.com>
2025-10-12 18:31:09 +0200

    add Position, Rotation and Scale to info node

--- source/blender/nodes/composite/nodes/node_composite_strip_info.cc
+++ source/blender/nodes/composite/nodes/node_composite_strip_info.cc
@@ -22,6 +22,9 @@ static void cmp_node_strip_info_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Int>("Start Frame").default_value(0);
   b.add_output<decl::Int>("End Frame").default_value(0);
+  b.add_output<decl::Vector>("Position").default_value({0, 0});
+  b.add_output<decl::Float>("Rotation").default_value(0.0);
+  b.add_output<decl::Vector>("Scale").default_value({0, 0});
 }
 
 using namespace blender::compositor;
@@ -34,13 +37,26 @@ class StripInfoOperation : public NodeOperation {
   {
     Result &result_start = get_result("Start Frame");
     Result &result_end = get_result("End Frame");
+    Result &result_position = get_result("Position");
+    Result &result_rotation = get_result("Rotation");
+    Result &result_scale = get_result("Scale");
 
     result_start.allocate_single_value();
     result_end.allocate_single_value();
+    result_position.allocate_single_value();
+    result_rotation.allocate_single_value();
+    result_scale.allocate_single_value();
 
     Strip strip = context().get_strip();
+
     result_start.set_single_value(static_cast<int>(strip.start + strip.startofs));
     result_end.set_single_value(strip.enddisp);
+
+    result_position.set_single_value(
+        float2(strip.data->transform->xofs, strip.data->transform->yofs));
+    result_rotation.set_single_value(strip.data->transform->rotation);
+    result_scale.set_single_value(
+        float2(strip.data->transform->scale_x, strip.data->transform->scale_y));
   }
 };
 
