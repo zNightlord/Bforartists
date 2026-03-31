815059977bf4be20a8f8a8be0a5e2432bccf327d
Omar Emara <mail@OmarEmara.dev>
2026-03-31 12:30:08 +0200

    Compositor: Add Object Info node
    This patch ports the Object Info node from Geometry Nodes to the
    compositor. Only the relevant inputs/outputs/options are available for
    the compositor. The rotation output is not enabled for now, but will be
    once !154845 lands.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/156428

--- scripts/startup/bl_ui/node_add_menu_compositor.py
+++ scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -60,6 +60,7 @@ class NODE_MT_compositor_node_input_scene_base(node_add_menu.NodeMenu):
         layout = self.layout
         self.node_operator(layout, "GeometryNodeInputActiveCamera")
         self.node_operator(layout, "GeometryNodeCameraInfo")
+        self.node_operator(layout, "GeometryNodeObjectInfo")
         if context.space_data.node_tree_sub_type == 'SCENE':
             self.node_operator(layout, "CompositorNodeRLayers")
         self.node_operator_with_outputs(context, layout, "CompositorNodeSceneTime", ["Frame", "Seconds"])
