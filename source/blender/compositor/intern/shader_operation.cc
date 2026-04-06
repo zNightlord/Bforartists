f7f5042dce166e47c83b7f73baf3bf4159b87fb8
Omar Emara <mail@OmarEmara.dev>
2026-04-06 12:45:57 +0200

    Fix #156786: Invalid links crash GPU compositor
    The GPU crashes on invalid links because no implicit conversion exists
    for the link. To fix this, we check if conversion is possible first and
    link a zero constant if not possible.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/156924

--- source/blender/compositor/intern/shader_operation.cc
+++ source/blender/compositor/intern/shader_operation.cc
@@ -30,6 +30,7 @@
 
 #include "BKE_node.hh"
 #include "BKE_node_runtime.hh"
+#include "BKE_type_conversions.hh"
 
 #include "COM_context.hh"
 #include "COM_pixel_operation.hh"
@@ -570,6 +571,16 @@ void ShaderOperation::convert_input_link_type(const bNodeSocket &input, const bN
 
   ShaderNode &input_node = *shader_nodes_.lookup(&input.owner_node());
   GPUNodeStack &input_stack = input_node.get_input(input.identifier);
+
+  /* Conversion is not possible, link a zero constant instead. */
+  const bke::DataTypeConversions &conversions = bke::get_implicit_type_conversions();
+  if (!conversions.is_convertible(Result::cpp_type(source_type), Result::cpp_type(target_type))) {
+    const char *function_name = get_set_function_name(target_type);
+    static const float zero_data[GPU_MAX_CONSTANT_DATA] = {0.0f};
+    GPU_link(material_, function_name, GPU_constant(zero_data), &input_stack.link);
+    return;
+  }
+
   const UString function_name = UString(
       fmt::format("{}_to_{}", Result::type_name(source_type), Result::type_name(target_type)));
 
