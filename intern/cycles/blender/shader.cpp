MaiKuraki/blender

commit defa2e3ddb98dcc36d987ae0413635f7e63d157b

Nodes: Add Boolean, Integer, and Vector input nodes in Shader Editor
This patch adds support for input nodes (Boolean, Integer, Vector)
in the Shader Node Editor, for both EEVEE and Cycles.

These nodes are built on existing `FunctionNode` definitions that
Geometry Nodes and the Compositor already use. Using a common definition
gives the advantage of being able to copy-paste them across all three
editors.

Pull Request: https://projects.blender.org/blender/blender/pulls/140911

intern/cycles/blender/shader.cpp
@@ -359,9 +359,24 @@ static ShaderNode *add_node(Scene *scene,
     color->set_value(get_node_output_rgba(b_node, "Color"));
     node = color;
   }
+  else if (b_node.is_type("FunctionNodeInputVector")) {
+    ColorNode *color = graph->create_node<ColorNode>();
+    color->set_value(get_node_output_vector(b_node, "Vector"));
+    node = color;
+  }
   else if (b_node.is_type("ShaderNodeValue")) {
     ValueNode *value = graph->create_node<ValueNode>();
     value->set_value(get_node_output_value(b_node, "Value"));
+    node = value;
+  }
+  else if (b_node.is_type("FunctionNodeInputBool")) {
+    ValueNode *value = graph->create_node<ValueNode>();
+    value->set_value(get_node_output_value(b_node, "Boolean"));
+    node = value;
+  }
+  else if (b_node.is_type("FunctionNodeInputInt")) {
+    ValueNode *value = graph->create_node<ValueNode>();
+    value->set_value(get_node_output_value(b_node, "Integer"));
     node = value;
   }
   else if (b_node.is_type("ShaderNodeCameraData")) {

source/blender/nodes/intern/shader_nodes_inline.cc
@@ -1380,27 +1380,25 @@ class ShaderNodesInliner {
       return {node, socket};
     }
     if (const int *value_int = std::get_if<int>(&value.value)) {
-      bNode *node = this->add_node("ShaderNodeValue");
+      bNode *node = this->add_node("FunctionNodeInputInt");
       bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
-      socket->default_value_typed<bNodeSocketValueFloat>()->value = *value_int;
+      socket->default_value_typed<bNodeSocketValueInt>()->value = *value_int;
       return {node, socket};
     }
     if (const bool *value_bool = std::get_if<bool>(&value.value)) {
-      bNode *node = this->add_node("ShaderNodeValue");
+      bNode *node = this->add_node("FunctionNodeInputBool");
       bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
-      socket->default_value_typed<bNodeSocketValueFloat>()->value = *value_bool;
+      socket->default_value_typed<bNodeSocketValueBoolean>()->value = *value_bool;
       return {node, socket};
     }
     if (const float3 *value_float3 = std::get_if<float3>(&value.value)) {
-      bNode *node = this->add_node("ShaderNodeCombineXYZ");
-      bNodeSocket *output_socket = static_cast<bNodeSocket *>(node->outputs.first);
-      bNodeSocket *input_x = static_cast<bNodeSocket *>(node->inputs.first);
-      bNodeSocket *input_y = input_x->next;
-      bNodeSocket *input_z = input_y->next;
-      input_x->default_value_typed<bNodeSocketValueFloat>()->value = value_float3->x;
-      input_y->default_value_typed<bNodeSocketValueFloat>()->value = value_float3->y;
-      input_z->default_value_typed<bNodeSocketValueFloat>()->value = value_float3->z;
-      return {node, output_socket};
+      bNode *node = this->add_node("FunctionNodeInputVector");
+      bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
+      bNodeSocketValueVector *vector_socket =
+          socket->default_value_typed<bNodeSocketValueVector>();
+      copy_v3_v3(vector_socket->value, *value_float3);
+      vector_socket->dimensions = 3;
+      return {node, socket};
     }
     if (const ColorGeometry4f *value_color = std::get_if<ColorGeometry4f>(&value.value)) {
       bNode *node = this->add_node("ShaderNodeRGB");

scripts/startup/bl_ui/node_add_menu_shader.py
@@ -56,6 +56,7 @@ class NODE_MT_shader_node_input_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "ShaderNodeAmbientOcclusion")
         self.node_operator(layout, "ShaderNodeAttribute")
         self.node_operator(layout, "ShaderNodeBevel")
+        self.node_operator(layout, "FunctionNodeInputBool")
         self.node_operator_with_outputs(
             context, layout, "ShaderNodeCameraData",
             ["View Vector", "View Z Depth", "View Distance"],
@@ -83,6 +84,7 @@ class NODE_MT_shader_node_input_base(node_add_menu.NodeMenu):
                 "Random Per Island",
             ],
         )
+        self.node_operator(layout, "FunctionNodeInputInt")
         self.node_operator(layout, "ShaderNodeLayerWeight")
         self.node_operator_with_outputs(
             context,
@@ -127,6 +129,7 @@ class NODE_MT_shader_node_input_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "ShaderNodeUVAlongStroke", poll=line_style_shader_nodes_poll(context))
         self.node_operator(layout, "ShaderNodeUVMap")
         self.node_operator(layout, "ShaderNodeValue")
+        self.node_operator(layout, "FunctionNodeInputVector")
         self.node_operator_with_outputs(
             context, layout, "ShaderNodeVolumeInfo",
             ["Color", "Density", "Flame", "Temperature"],

source/blender/nodes/function/CMakeLists.txt
@@ -7,6 +7,7 @@ set(INC
   ..
   include
   ../intern
+  ../shader
   ../../editors/include
   ../../makesrna
   # RNA_prototypes.hh
@@ -81,6 +82,7 @@ set(LIB
   PRIVATE bf::blenloader
   PRIVATE bf::dna
   PRIVATE bf::functions
+  PRIVATE bf::gpu
   PRIVATE bf::imbuf
   PRIVATE bf::intern::guardedalloc
   PRIVATE bf::windowmanager

source/blender/nodes/function/nodes/node_fn_input_bool.cc
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "node_function_util.hh"
+#include "node_shader_util.hh"
 
 #include "NOD_geometry_nodes_gizmos.hh"
 
@@ -24,12 +24,32 @@ static void node_declare(NodeDeclarationBuilder &b)
   });
 }
 
+static int gpu_shader_bool(GPUMaterial *mat,
+                           bNode *node,
+                           bNodeExecData * /*execdata*/,
+                           GPUNodeStack * /*in*/,
+                           GPUNodeStack *out)
+{
+  NodeInputBool *node_storage = static_cast<NodeInputBool *>(node->storage);
+  float value = static_cast<float>(node_storage->boolean);
+  return GPU_link(mat, "set_value", GPU_uniform(&value), &out->link);
+}
+
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
   const bNode &bnode = builder.node();
   NodeInputBool *node_storage = static_cast<NodeInputBool *>(bnode.storage);
   builder.construct_and_set_matching_fn<mf::CustomMF_Constant<bool>>(node_storage->boolean);
 }
+
+NODE_SHADER_MATERIALX_BEGIN
+#ifdef WITH_MATERIALX
+{
+  NodeItem boolean = get_output_default("Boolean", NodeItem::Type::Boolean);
+  return create_node("constant", NodeItem::Type::Boolean, {{"value", boolean}});
+}
+#endif
+NODE_SHADER_MATERIALX_END
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
@@ -41,7 +61,7 @@ static void node_register()
 {
   static bke::bNodeType ntype;
 
-  fn_cmp_node_type_base(&ntype, "FunctionNodeInputBool", FN_NODE_INPUT_BOOL);
+  common_node_type_base(&ntype, "FunctionNodeInputBool", FN_NODE_INPUT_BOOL);
   ntype.ui_name = "Boolean";
   ntype.ui_description =
       "Provide a True/False value that can be connected to other nodes in the tree";
@@ -49,9 +69,12 @@ static void node_register()
   ntype.nclass = NODE_CLASS_INPUT;
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
+  ntype.gpu_fn = gpu_shader_bool;
   bke::node_type_storage(
       ntype, "NodeInputBool", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
+  ntype.materialx_fn = node_shader_materialx;
+
   bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/function/nodes/node_fn_input_int.cc
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "node_function_util.hh"
+#include "node_shader_util.hh"
 
 #include "UI_interface_layout.hh"
 #include "UI_resources.hh"
@@ -24,12 +24,32 @@ static void node_declare(NodeDeclarationBuilder &b)
   ;
 }
 
+static int gpu_shader_int(GPUMaterial *mat,
+                          bNode *node,
+                          bNodeExecData * /*execdata*/,
+                          GPUNodeStack * /*in*/,
+                          GPUNodeStack *out)
+{
+  NodeInputInt *node_storage = static_cast<NodeInputInt *>(node->storage);
+  float integer = static_cast<float>(node_storage->integer);
+  return GPU_link(mat, "set_value", GPU_uniform(&integer), &out->link);
+}
+
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
   const bNode &bnode = builder.node();
   NodeInputInt *node_storage = static_cast<NodeInputInt *>(bnode.storage);
   builder.construct_and_set_matching_fn<mf::CustomMF_Constant<int>>(node_storage->integer);
 }
+
+NODE_SHADER_MATERIALX_BEGIN
+#ifdef WITH_MATERIALX
+{
+  NodeItem integer = get_output_default("Integer", NodeItem::Type::Integer);
+  return create_node("constant", NodeItem::Type::Integer, {{"value", integer}});
+}
+#endif
+NODE_SHADER_MATERIALX_END
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
@@ -41,7 +61,7 @@ static void node_register()
 {
   static bke::bNodeType ntype;
 
-  fn_cmp_node_type_base(&ntype, "FunctionNodeInputInt", FN_NODE_INPUT_INT);
+  common_node_type_base(&ntype, "FunctionNodeInputInt", FN_NODE_INPUT_INT);
   ntype.ui_name = "Integer";
   ntype.ui_description =
       "Provide an integer value that can be connected to other nodes in the tree";
@@ -49,9 +69,12 @@ static void node_register()
   ntype.nclass = NODE_CLASS_INPUT;
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
+  ntype.gpu_fn = gpu_shader_int;
   bke::node_type_storage(
       ntype, "NodeInputInt", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
+  ntype.materialx_fn = node_shader_materialx;
+
   bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/function/nodes/node_fn_input_vector.cc
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "node_function_util.hh"
+#include "node_shader_util.hh"
 
 #include "NOD_geometry_nodes_gizmos.hh"
 
@@ -30,6 +30,25 @@ static void node_declare(NodeDeclarationBuilder &b)
         }
       });
 }
+
+static int gpu_shader_vector(GPUMaterial *mat,
+                             bNode *node,
+                             bNodeExecData * /*execdata*/,
+                             GPUNodeStack * /*in*/,
+                             GPUNodeStack *out)
+{
+  NodeInputVector *node_storage = static_cast<NodeInputVector *>(node->storage);
+  return GPU_link(mat, "set_rgb", GPU_uniform(node_storage->vector), &out->link);
+}
+
+NODE_SHADER_MATERIALX_BEGIN
+#ifdef WITH_MATERIALX
+{
+  NodeItem vector = get_output_default("Vector", NodeItem::Type::Vector3);
+  return create_node("constant", NodeItem::Type::Vector3, {{"value", vector}});
+}
+#endif
+NODE_SHADER_MATERIALX_END
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
@@ -71,17 +90,20 @@ static void node_register()
 {
   static bke::bNodeType ntype;
 
-  fn_cmp_node_type_base(&ntype, "FunctionNodeInputVector", FN_NODE_INPUT_VECTOR);
+  common_node_type_base(&ntype, "FunctionNodeInputVector", FN_NODE_INPUT_VECTOR);
   ntype.ui_name = "Vector";
   ntype.ui_description = "Provide a vector value that can be connected to other nodes in the tree";
   ntype.enum_name_legacy = "INPUT_VECTOR";
   ntype.nclass = NODE_CLASS_INPUT;
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
+  ntype.gpu_fn = gpu_shader_vector;
   ntype.draw_buttons_ex = node_layout_ex;
   bke::node_type_storage(
       ntype, "NodeInputVector", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
+  ntype.materialx_fn = node_shader_materialx;
+
   bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

tests/files/render/shader/cycles_renders/input_boolean.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb7a55ca0d2cf1c5a04f0769ebce36bf3435969fef13ba35915a753f0bf3b6e4
+size 10545

zNightlord/Bforartists

commit 9d766c7799c92070f5a1f3dd79b04ade8dd67208

Fix #156483: Raycast node does not work properly if a direction is specified
Constant value is stored in node storage instead of output socket.

Additionally, the Vector Input node has output socket called "Vector",
which needs to be remapped to "Color" for Cycles to match ColorNode.

Broken in defa2e3ddb98dcc36d987ae0413635f7e63d157b.

Co-authored-by: Sergey Sharybin <sergey@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/156493

intern/cycles/blender/shader.cpp
@@ -361,9 +361,10 @@ static ShaderNode *add_node(Scene *scene,
     node = color;
   }
   else if (b_node.is_type("FunctionNodeInputVector")) {
-    ColorNode *color = graph->create_node<ColorNode>();
-    color->set_value(get_node_output_vector(b_node, "Vector"));
-    node = color;
+    ColorNode *value = graph->create_node<ColorNode>();
+    const auto &storage = *static_cast<const blender::NodeInputVector *>(b_node.storage);
+    value->set_value(make_float3(storage.vector[0], storage.vector[1], storage.vector[2]));
+    node = value;
   }
   else if (b_node.is_type("ShaderNodeValue")) {
     ValueNode *value = graph->create_node<ValueNode>();
@@ -372,12 +373,14 @@ static ShaderNode *add_node(Scene *scene,
   }
   else if (b_node.is_type("FunctionNodeInputBool")) {
     ValueNode *value = graph->create_node<ValueNode>();
-    value->set_value(get_node_output_value(b_node, "Boolean"));
+    const auto &storage = *static_cast<const blender::NodeInputBool *>(b_node.storage);
+    value->set_value(bool(storage.boolean));
     node = value;
   }
   else if (b_node.is_type("FunctionNodeInputInt")) {
     ValueNode *value = graph->create_node<ValueNode>();
-    value->set_value(get_node_output_value(b_node, "Integer"));
+    const auto &storage = *static_cast<const blender::NodeInputInt *>(b_node.storage);
+    value->set_value(storage.integer);
     node = value;
   }
   else if (b_node.is_type("ShaderNodeCameraData")) {
@@ -1228,6 +1231,14 @@ static ShaderOutput *node_find_output_by_name(blender::bNode &b_node,
       }
       else if (string_endswith(name, "Result_Vector")) {
         string_replace(name, "Result_Vector", "Result");
+        output = node->output(name.c_str());
+      }
+    }
+    else if (b_node.is_type("FunctionNodeInputVector")) {
+      /* FunctionNodeInputVector has an output called "Vector", and it uses ColorNode Cycles node
+       * that has an output called "Color". */
+      if (name == "Vector") {
+        name = "Color";
         output = node->output(name.c_str());
       }
     }

source/blender/nodes/intern/shader_nodes_inline.cc
@@ -1395,24 +1395,22 @@ class ShaderNodesInliner {
     }
     if (const int *value_int = std::get_if<int>(&value.value)) {
       bNode *node = this->add_node("FunctionNodeInputInt");
-      bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
-      socket->default_value_typed<bNodeSocketValueInt>()->value = *value_int;
-      return {node, socket};
+      auto &storage = *static_cast<NodeInputInt *>(node->storage);
+      storage.integer = *value_int;
+      return {node, static_cast<bNodeSocket *>(node->outputs.first)};
     }
     if (const bool *value_bool = std::get_if<bool>(&value.value)) {
       bNode *node = this->add_node("FunctionNodeInputBool");
-      bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
-      socket->default_value_typed<bNodeSocketValueBoolean>()->value = *value_bool;
-      return {node, socket};
+      auto &storage = *static_cast<NodeInputBool *>(node->storage);
+      storage.boolean = int(*value_bool);
+      return {node, static_cast<bNodeSocket *>(node->outputs.first)};
     }
     if (const float3 *value_float3 = std::get_if<float3>(&value.value)) {
       bNode *node = this->add_node("FunctionNodeInputVector");
-      bNodeSocket *socket = static_cast<bNodeSocket *>(node->outputs.first);
-      bNodeSocketValueVector *vector_socket =
-          socket->default_value_typed<bNodeSocketValueVector>();
-      copy_v3_v3(vector_socket->value, *value_float3);
-      vector_socket->dimensions = 3;
-      return {node, socket};
+      auto &storage = *static_cast<NodeInputVector *>(node->storage);
+      copy_v3_v3(storage.vector, *value_float3);
+      storage.dimensions = 3;
+      return {node, static_cast<bNodeSocket *>(node->outputs.first)};
     }
     if (const ColorGeometry4f *value_color = std::get_if<ColorGeometry4f>(&value.value)) {
       bNode *node = this->add_node("ShaderNodeRGB");

tests/files/render/raycast/cycles_renders/raycast_direction_constant.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f753f976693b9f917fae4ca522939271b665c4d356d457ede8c705649ad96e4
+size 1729

tests/files/render/raycast/eevee_renders/raycast_direction_constant.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1bd7f65b412ae92bf29bc6f25e987f9d4cd66bb59cbe45926d81d6ef1a573f76
+size 1892

tests/files/render/raycast/raycast_direction_constant.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5883b8b615ce79539036bbf9cc003042e77fea9fd4179b2ec7ad803344ee8e96
+size 97099

tests/files/render/raycast/storm_hydra_renders/raycast_direction_constant.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eb8e922edfe39ddedd578257a4aee329a32a96c4857e4f56bf5040684fb5287
+size 748

tests/files/render/raycast/storm_usd_renders/raycast_direction_constant.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14be681e143e654731bd7d2f82a1d96e66d96fa2715f8c58cdf6c88d412eb39a
+size 748