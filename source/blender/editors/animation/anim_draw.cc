blender/blender

commit 1adc36f5233e03172bb7619562f0d0e056856e57

Fix #150036: FCurve normalization with preview range in empty area
When enabling normalization outside the keyed range of an FCurve
it would disappear. The reason was that the default value of
`-FLT_MAX` was used as an offset in that case.
Just returning that as 0 fixes the issue.

Pull Request: https://projects.blender.org/blender/blender/pulls/150078

source/blender/editors/animation/anim_draw.cc
@@ -608,7 +608,7 @@ static float normalization_factor_get(Scene *scene, FCurve *fcu, short flag, flo
   else {
     /* Skip normalization. */
     factor = 1.0f;
-    offset = -min_coord;
+    offset = 0.0f;
   }
 
   BLI_assert(factor != 0.0f);

zNightlord/Bforartists

commit 121cfcfe112d707da6789273d427a988181bd1c5

Fix #156273: Normalization in Graph Editor error with flat curves
When using normalization in the Graph Editor, flat curves
were not put back to the 0 line. This was caused by a fix
to make sure curves without any keys in the given range don't disappear.

The fix is to respect both cases.
Either `fcurve_scene_coord_range_get` finds no keys in which case
`min_coord` is still `FLT_MAX` or there are keys but they are all flat
in which case `min_coord` is a usable value and should be set.

Caused by 1adc36f5233e03172bb7619562f0d0e056856e57

Pull Request: https://projects.blender.org/blender/blender/pulls/156343

source/blender/editors/animation/anim_draw.cc
@@ -609,9 +609,16 @@ static float normalization_factor_get(Scene *scene, FCurve *fcu, short flag, flo
     offset = -min_coord - range / 2.0f;
   }
   else {
-    /* Skip normalization. */
+    /* Skip normalization in 2 cases. Either the y difference of all keyframes is too small to
+     * normalize or there are no keys at all in the range. In the first case, the curve should be
+     * brought to the 0 line. In the second case we cannot do that since we have no information. */
     factor = 1.0f;
-    offset = 0.0f;
+    if (min_coord == FLT_MAX) {
+      offset = 0.0f;
+    }
+    else {
+      offset = -min_coord;
+    }
   }
 
   BLI_assert(factor != 0.0f);