zNightlord/Bforartists

commit f082decf64112c33a6f1c0aaefed603c46046c21

Fix #156370: Broken render in Offscreen.draw_view3d()
Regression from 927cfc6d172f49191cd492f091a91fa17ff93b19.
(See that commit message for insight into previous related issues)

Some offscreen render paths have the same issue as XR of relying on a
custom projection matrix.
`ED_view3d_draw_offscreen`"flagged" the v3d as such, by setting the
lens to 0, but we no longer check that.

This replaces that workaround with a dedicated `V3D_CUSTOM_MATRIX` flag
and updates EEVEE to check it.

Pull Request: https://projects.blender.org/blender/blender/pulls/156389

source/blender/editors/space_view3d/view3d_draw.cc
@@ -1871,7 +1871,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
     /* #View3D */
     eDrawType v3d_shading_type;
     Object *v3d_camera;
-    float v3d_lens;
+    short v3d_flag;
 
     /* #Region */
     int region_winx, region_winy;
@@ -1887,7 +1887,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
   } orig{};
   orig.v3d_shading_type = eDrawType(v3d->shading.type);
   orig.v3d_camera = v3d->camera;
-  orig.v3d_lens = v3d->lens;
+  orig.v3d_flag = v3d->flag;
   orig.region_winx = region->winx;
   orig.region_winy = region->winy;
   orig.region_winrct = region->winrct;
@@ -1918,16 +1918,6 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
     BKE_image_free_anim_gputextures(G.main);
   }
 
-  if (viewmat) {
-    /* WORKAROUND: Disable camera view to avoid EEVEE being confused and try to
-     * get the projection matrix from the camera.
-     * Set the `lens` parameter to 0 to make EEVEE prefer the `winmat` from the rv3d instead of
-     * trying to rederive it. Note that this produces incorrect result with over-scan. */
-    rv3d->persp = (winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
-    v3d->camera = nullptr;
-    v3d->lens = 0.0f;
-  }
-
   GPU_matrix_push_projection();
   GPU_matrix_identity_set();
   GPU_matrix_push();
@@ -1940,6 +1930,15 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
   }
   else {
     view3d_main_region_setup_offscreen(depsgraph, scene, v3d, region, viewmat, winmat);
+  }
+
+  if (viewmat || winmat) {
+    /* Now that rv3d data has been updated taking into account viewmat and winmat,
+     * we can remove the camera and flag the view as using custom matrices,
+     * to ensure engines don't recompute them. */
+    v3d->camera = nullptr;
+    v3d->flag |= V3D_CUSTOM_MATRIX;
+    rv3d->persp = (winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
   }
 
   if (viewport) {
@@ -1978,7 +1977,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
 
   v3d->shading.type = orig.v3d_shading_type;
   v3d->camera = orig.v3d_camera;
-  v3d->lens = orig.v3d_lens;
+  v3d->flag = orig.v3d_flag;
 
   G.f &= ~G_FLAG_RENDER_VIEWPORT;
 }

source/blender/draw/engines/eevee/eevee_camera.cc
@@ -76,8 +76,9 @@ void Camera::init()
   float overscan = 0.0f;
   if ((inst_.scene->eevee.flag & SCE_EEVEE_OVERSCAN) && (inst_.drw_view || inst_.render)) {
     overscan = inst_.scene->eevee.overscan / 100.0f;
-    if (inst_.is_xr()) {
-      /* In XR we need to use the v3d winmat as-is. */
+    if (inst_.is_custom_matrix()) {
+      /* If using a custom matrix (XR and some offscreen render paths)
+       * we need to use the v3d winmat as-is. */
       overscan = 0.0f;
     }
   }
@@ -137,8 +138,9 @@ void Camera::sync()
     data.viewmat = inst_.drw_view->viewmat();
     data.viewinv = inst_.drw_view->viewinv();
 
-    if (inst_.is_xr()) {
-      /* In XR we need to use the v3d winmat as-is. */
+    if (inst_.is_custom_matrix()) {
+      /* If using a custom matrix (XR and some offscreen render paths)
+       * we need to use the v3d winmat as-is. */
       data.winmat = inst_.drw_view->winmat();
       data.wininv = inst_.drw_view->wininv();
     }

source/blender/draw/engines/eevee/eevee_instance.hh
@@ -24,6 +24,7 @@
 
 #include "DNA_lightprobe_types.h"
 
+#include "DNA_view3d_types.h"
 #include "DRW_render.hh"
 
 #include "eevee_ambient_occlusion.hh"
@@ -314,9 +315,10 @@ class Instance : public DrawEngine {
     return is_light_bake;
   }
 
-  bool is_xr() const
+  bool is_custom_matrix() const
   {
-    return draw_ctx && draw_ctx->mode == DRWContext::VIEWPORT_XR;
+    return (v3d && v3d->flag & V3D_CUSTOM_MATRIX) ||
+           (draw_ctx && draw_ctx->mode == DRWContext::VIEWPORT_XR);
   }
 
   bool overlays_enabled() const

source/blender/makesdna/DNA_view3d_types.h
@@ -54,6 +54,7 @@ enum {
   V3D_FLAG_UNUSED_2 = 1 << 3, /* cleared */
   V3D_XR_SESSION_MIRROR = 1 << 4,
   V3D_XR_SESSION_SURFACE = 1 << 5,
+  V3D_CUSTOM_MATRIX = 1 << 6,
 
   V3D_FLAG_UNUSED_10 = 1 << 10, /* cleared */
   V3D_SELECT_OUTLINE = 1 << 11,

commit 0380e5c0b0b81f08ff7d8e92d80fe8c5d2850def

Fix: ED_view3d_draw_offscreen null dereference
Regression from f082decf64.

The winmat parameter might be null, but `rv3d->winmat` is guaranteed to
be the final one at this point.

Pull Request: https://projects.blender.org/blender/blender/pulls/157294

source/blender/editors/space_view3d/view3d_draw.cc
@@ -1938,7 +1938,7 @@ void ED_view3d_draw_offscreen(Depsgraph *depsgraph,
      * to ensure engines don't recompute them. */
     v3d->camera = nullptr;
     v3d->flag |= V3D_CUSTOM_MATRIX;
-    rv3d->persp = (winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
+    rv3d->persp = (rv3d->winmat[3][3] == 0.0f) ? RV3D_PERSP : RV3D_ORTHO;
   }
 
   if (viewport) {