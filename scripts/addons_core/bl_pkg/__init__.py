4aed132e7c5b5c56a2130482b465815e9f143eed
Sybren A. Stüvel <sybren@blender.org>
2026-04-14 18:01:20 +0200

    Assets: auto-download the listing for enabled remote libraries only
    Skip the daily auto-downloading of asset listings when the library has
    been disabled in the preferences.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/157356

--- scripts/addons_core/bl_pkg/__init__.py
+++ scripts/addons_core/bl_pkg/__init__.py
@@ -536,6 +536,8 @@ def _remote_asset_library_sync_all_periodic():
         return
 
     for asset_lib in bpy.context.preferences.filepaths.asset_libraries:
+        if not asset_lib.enabled:
+            continue
         remote_asset_library_sync(asset_lib.remote_url, Path(asset_lib.path),
                                   only_if_older_than_sec=REMOTE_ASSET_LIBS_AUTOSYNC_PERIOD_SEC)
 
