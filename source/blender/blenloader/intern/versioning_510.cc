503a93d29aa2e3887269f61ae27c66d33ce55041
Trung <0znightlord0@gmail.com>
2026-05-05 12:14:24 +0700

    node versioning

--- source/blender/blenloader/intern/versioning_510.cc
+++ source/blender/blenloader/intern/versioning_510.cc
@@ -923,7 +923,7 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     }
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 27)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 21)) {
     for (bScreen &screen : bmain->screens) {
       for (ScrArea &area : screen.areabase) {
         for (SpaceLink &space : area.spacedata) {
