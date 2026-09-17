9367b870bc35c567d537c06d275b4f7559d119ba
Trung Phạm <61040487+zNightlord@users.noreply.github.com>
2026-09-17 23:37:05 +0700

    Change padding size for bone handle structure
    Reduced padding size from 3 bytes to 1 byte in DNA_armature_types.h.

--- source/blender/makesdna/DNA_armature_types.h
+++ source/blender/makesdna/DNA_armature_types.h
@@ -391,7 +391,7 @@ struct Bone {
   /** Type of next/prev bone handles. */
   eBone_BBoneHandleType bbone_prev_type = {};
   eBone_BBoneHandleType bbone_next_type = {};
-  char _pad2[3] = {};
+  char _pad2[1] = {};
 
   /** B-Bone flags. */
   eBone_BBoneFlag bbone_flag = {};
